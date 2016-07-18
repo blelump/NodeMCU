3d883bdb85fd5a5a1b51c5ce552cd9ca75540b39
blelump <blelump@martex.nom.pl>
2016-07-18 10:29:32 +0200

    Another version of irrigation algorithm

--- tomatoes/app.lua
+++ tomatoes/app.lua
@@ -39,19 +39,25 @@ local function start_measurements()
 
                 local running, mode = tmr.state(3)
                 local series = 0
+                local check_water = barrel.check_water()
                 if not running then
-                    database.insert('events', { irrigation_started = 1 })
-                    tmr.alarm(3, config.IRRIGATION_INTERVAL, tmr.ALARM_AUTO, function()
-                        if barrel.check_water() then
-                            if series <= 2 then
-                                gpio.write(config.RELAY_PIN, gpio.HIGH)
-                                database.insert('events', { irrigation_interval_performed = 1 })
-                            else
+                    if check_water then
+                        database.insert('events', { irrigation_started = 1 })
+                    end
+                    tmr.alarm(3, config.DELAY_INTERVAL, tmr.ALARM_AUTO, function()
+
+                        if check_water then
+                            gpio.write(config.RELAY_PIN, gpio.HIGH)
+                            tmr.alarm(4, config.IRRIGATION_INTERVAL, tmr.ALARM_SINGLE, function()
                                 gpio.write(config.RELAY_PIN, gpio.LOW)
-                                database.insert('events', { irrigation_performed = 1 })
-                                tmr.unregister(3)
-                                soil_humidity.reset()
-                            end
+                                if series <= config.IRRIGATION_SERIES then
+                                    database.insert('events', { irrigation_interval_performed = 1 })
+                                else
+                                    database.insert('events', { irrigation_performed = 1 })
+                                    tmr.unregister(3)
+                                    soil_humidity.reset()
+                                end
+                            end)
                         else
                             database.insert('events', { not_enough_water = 1 })
                             gpio.write(config.RELAY_PIN, gpio.LOW)
